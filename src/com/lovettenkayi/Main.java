dd034bcecdcf7f4a81fbce0288bd8881eb8ed71e
nkayilovette03 <nkayilovette03@gmail.com>
2024-03-12 14:43:15 -0400

    Non-primitive date types

--- src/com/lovettenkayi/Main.java
+++ src/com/lovettenkayi/Main.java
@@ -1,29 +1,37 @@
 package com.lovettenkayi;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-//       Primitive Data Types
-        byte theByte = -128;
-        short theShort = 32_000;
-        int theInt = 787_838_899;
-        long theLong = 769_876_555;
+        // Primitive Data Types
 
-        float pi = 3.14F;
-        double doublePi = 3.1416;
-        boolean isAdult = true;
+//        byte theByte = -128;
+//        short theShort = 32_000;
+//        int theInt = 787_838_899;
+//        long theLong = 769_876_555;
+//
+//        float pi = 3.14F;
+//        double doublePi = 3.1416;
+//        boolean isAdult = true;
+//
+//        char nameInitial = 'L';
+//
+//        System.out.println(theByte);
+//        System.out.println(theShort);
+//        System.out.println(theInt);
+//        System.out.println(theLong);
+//        System.out.println(pi);
+//        System.out.println(doublePi);
+//        System.out.println(isAdult);
+//        System.out.println(nameInitial);
 
-        char nameInitial = 'L';
+        // Non-Primitive Data Types AKA reference types
+        String name = new String("Lovette Nkayi");
 
-        System.out.println(theByte);
-        System.out.println(theShort);
-        System.out.println(theInt);
-        System.out.println(theLong);
-        System.out.println(pi);
-        System.out.println(doublePi);
-        System.out.println(isAdult);
-        System.out.println(nameInitial);
+        LocalDateTime dateTime = LocalDateTime.now();
 
-//        Nice learning this
+        System.out.println(name.toUpperCase());
+        System.out.println(dateTime.getMonth());
     }
 }
